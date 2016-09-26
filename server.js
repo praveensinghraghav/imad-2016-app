e56b87e44b4498e6e3fb57a756607715d09ba390
Praveen Kumar <praveensinghraghav@users.noreply.github.com>
2016-09-26 22:26:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,10 +93,6 @@ app.get('/:articleName', function(req, res) {
     res.send(createtemplate(articles[articleName]));
 });
 
-app.get('/article-three', function(req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
