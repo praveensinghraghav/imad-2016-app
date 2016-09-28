f3911a575b38b594b0d5f67284c4a189a250204a
Praveen Kumar <praveensinghraghav@users.noreply.github.com>
2016-09-28 19:09:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+//change text to main-text
+var element= document.getElementById('main');
+element.innerHTML= 'New Value';
+
+//move the image
+var img=document.getElementById('raghav');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'pix';
+}
+img.onclick = function()
+{
+    var interval = setInterval(moveRight , 100);
+};
